effc1a931b2881ed4fa1b97cbdb4708b9451f3c7
Michał Ciesielski <majalcmaj@gmail.com>
2018-01-15 16:06:50 +0100

    modeified png2bin program

--- png2bin/src/main.c
+++ png2bin/src/main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "io_helpers/io_helpers.h"
 
-int p2b(const char* in_path, const char* out_path) 
+int p2b(const char* in_path, const char* out_path, int scale) 
 {
     rgb_image image;
     int res = 0;
@@ -22,19 +22,29 @@ int p2b(const char* in_path, const char* out_path)
         return 2;
     }
 
+    int new_height = image.height * scale;
     int img_field = image.width * image.height;
     fnres = fwrite(&image.width, sizeof(int), 1, outfile);
     if(fnres != 1) res = 3;
-    fnres = fwrite(&image.height, sizeof(int), 1, outfile);
+    fnres = fwrite(&new_height, sizeof(int), 1, outfile);
     if(fnres != 1) res = 3;
-    fnres = fwrite(image.r_channel, sizeof(byte), img_field, outfile);
-    if(fnres != img_field) res = 3;
-    fnres = fwrite(image.g_channel, sizeof(byte), img_field, outfile);
-    if(fnres != img_field) res = 3;
-    fnres = fwrite(image.b_channel, sizeof(byte), img_field, outfile);
-    if(fnres != img_field) res = 3;
-    fnres = fwrite(image.alpha_channel, sizeof(byte), img_field, outfile);
-    if(fnres != img_field) res = 3;
+    for(int i = 0 ; i < scale ; i++) {
+        fnres = fwrite(image.r_channel, sizeof(byte), img_field, outfile);
+        if(fnres != img_field) res = 3;
+    }
+
+    for(int i = 0 ; i < scale ; i++) {
+        fnres = fwrite(image.g_channel, sizeof(byte), img_field, outfile);
+        if(fnres != img_field) res = 3;
+    } 
+    for(int i = 0 ; i < scale ; i++) {
+        fnres = fwrite(image.b_channel, sizeof(byte), img_field, outfile);
+        if(fnres != img_field) res = 3;
+    }
+    for(int i = 0 ; i < scale ; i++) {
+        fnres = fwrite(image.alpha_channel, sizeof(byte), img_field, outfile);
+        if(fnres != img_field) res = 3;
+    }
     fclose(outfile);
     image_dispose(image);
     return res;
@@ -88,14 +98,20 @@ int b2p(const char* in_path, const char* out_path)
 
 int main(int argc, const char** argv)
 {
-    if(argc != 4) {
-        fprintf(stderr, "Exactly 3 arguments should be provided: MODE (p2b - png to bin or b2p - bin to png) IN_FILE OUT_FILE\n");
+    if(argc != 4 && argc != 5) {
+        fprintf(stderr, "3 or 4 arguments should be provided: MODE (p2b - png to bin or b2p - bin to png) IN_FILE OUT_FILE [OUT_SCALE - only for p2b, default 1]\n");
         return 3;
     }
 
     const char* mode = argv[1];
     if(strcmp(mode, "p2b") == 0) {
-        return p2b(argv[2], argv[3]);
+        int scale;
+        if(argc == 5) {
+            scale = atoi(argv[4]);
+        }else {
+            scale = 1;
+        }
+        return p2b(argv[2], argv[3], scale);
     } else if(strcmp(mode, "b2p") == 0) {
         return b2p(argv[2], argv[3]);
     } else {
